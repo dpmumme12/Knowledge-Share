bec5b31187eedc532a005e981500e4641d2edcf7
Doug Mumme <dougmumme@gmail.com>
2022-05-30 17:46:16 -0500

    updated settings

--- config/settings/ci_testing.py
+++ config/settings/ci_testing.py
@@ -13,8 +13,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'TestDB'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
