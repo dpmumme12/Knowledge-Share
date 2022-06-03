9a9814c2a2ebb51f31fffa6e01eea45125a12af4
Doug Mumme <dougmumme@gmail.com>
2022-06-02 21:03:19 -0500

    updated static root

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = APPS_DIR / 'static_root'
+STATIC_ROOT = APPS_DIR / 'static'
 
 STATICFILES_DIRS = [APPS_DIR / 'static']
 
